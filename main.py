85eced12cb67678313ef742e490d267d4ffae74b
thangatham <144529700+thangatham@users.noreply.github.com>
2023-09-25 19:48:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+#Define the base class playerclass player:
+class player:
+  def player(self):
+    print("The player is playing cricket.")
 
+#Define the derived class Batsman
+class Batsman(player):
+  def play(self):
+    print("The Batsman is batting.")
 
-number = int(input("enter a value:"))
-res = fact_rec(number)
-print("the factorial of {}is{}.".format(number, res))
+#Define the derived class Bowler
+class Bowler(player):
+  def play(self):
+    print("The bowler is bowling.")
+
+#create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#call the play() method for each object 
+batsman.play()
+bowler.play()
