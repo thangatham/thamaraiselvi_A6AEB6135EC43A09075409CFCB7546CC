db21a6a81b372ab2e91cb87b2e48e6df9597a93f
thangatham <144529700+thangatham@users.noreply.github.com>
2023-09-25 19:35:17 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,22 +1,32 @@
-#Define the base class playerclass player:
-class player:
-  def player(self):
-    print("The player is playing cricket.")
-
-#Define the derived class Batsman
-class Batsman(player):
-  def play(self):
-    print("The Batsman is batting.")
-
-#Define the derived class Bowler
-class Bowler(player):
-  def play(self):
-    print("The bowler is bowling.")
-
-#create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-
-#call the play() method for each object 
-batsman.play()
-bowler.play()
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
